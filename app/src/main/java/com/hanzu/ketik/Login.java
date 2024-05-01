fc927e3398d4e4134493b72b2a697306174a1554
hanzu <hammam2306@gmail.com>
2024-05-01 10:40:57 +0700

    added file

---
+++ app/src/main/java/com/hanzu/ketik/Login.java
@@ -0,0 +1,131 @@
+package com.hanzu.ketik;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    private static final String TAG = "Login";
+    private static final int REQUEST_SIGNUP = 0;
+
+    private Button btnlogin;
+    private EditText username, password;
+
+    SharedPreference sharedPreference;
+
+    Activity context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.txt_user);
+        password = (EditText) findViewById(R.id.txt_password);
+        btnlogin = (Button) findViewById(R.id.btn_login);
+
+        sharedPreference = new SharedPreference();
+
+        String name = sharedPreference.getValue(context);
+
+        if (name != null) {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    public void login(){
+        Log.d(TAG,"Login");
+
+        if (!validate()){
+            onLoginFailed();
+            return;
+        }
+
+        btnlogin.setEnabled(false);
+
+        final ProgressDialog progressDialog =
+                new ProgressDialog(Login.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String isiUser = username.getText().toString();
+        String isiPassword = password.getText().toString();
+
+        InputMethodManager imm =
+                (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(username.getWindowToken(),0);
+
+        sharedPreference.save(this, isiUser);
+        Toast.makeText(this, "Data berhasil disimpan", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onLoginSucces();
+                progressDialog.dismiss();
+            }
+        },3000);
+    }
+    public void onLoginSucces(){
+        Intent intent = new Intent(context, MainActivity.class);
+        startActivity(intent);
+        btnlogin.setEnabled(true);
+        finish();
+    }
+    public void  onLoginFailed(){
+        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+        btnlogin.setEnabled(true);
+    }
+    public boolean validate(){
+        boolean valid = true;
+
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+
+        if (user.isEmpty()){
+            username.setError("username can't be null");
+            valid = false;
+        }else {
+            username.setError(null);
+        }
+        if (pass.isEmpty()){
+            password.setError("Password can't be null");
+            valid = false;
+        }else {
+            password.setError(null);
+        }
+        return valid;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SIGNUP){
+            if (requestCode == RESULT_OK){
+                this.finish();
+            }
+        }
+    }
+}
